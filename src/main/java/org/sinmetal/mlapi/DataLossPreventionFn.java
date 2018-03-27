0413f99a80f8917631b8869d1b751d3315941c40
sinmetal <metal.tie@gmail.com>
2018-03-27 19:37:37 +0900

    DLP 適用Optionを増やした

--- src/main/java/org/sinmetal/mlapi/DataLossPreventionFn.java
+++ src/main/java/org/sinmetal/mlapi/DataLossPreventionFn.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.google.privacy.dlp.v2beta1.*;
 import org.apache.beam.sdk.transforms.DoFn;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
@@ -16,36 +17,31 @@ public class DataLossPreventionFn extends DoFn<TableRow, TableRow> {
 
     @ProcessElement
     public void processElement(ProcessContext c) {
+        Long id = parseLong(c.element().get("Id"));
         String message = c.element().get("Body").toString();
-        logger.info("hoge-message=" + message);
 
         DlpServiceClient dlp = null;
         try {
             dlp = DlpServiceClient.create();
-            String name = "EMAIL_ADDRESS";
-            InfoType infoTypesElement = InfoType.newBuilder()
-                    .setName(name)
-                    .build();
-            List<InfoType> infoTypes = Arrays.asList(infoTypesElement);
+
             InspectConfig inspectConfig = InspectConfig.newBuilder()
-                    .addAllInfoTypes(infoTypes)
+                    .addAllInfoTypes(getInfoTypeList())
                     .build();
             String type = "text/plain";
-            //String value = "My email is example @example.com.";
             ContentItem itemsElement = ContentItem.newBuilder()
                     .setType(type)
                     .setValue(message)
                     .build();
             List<ContentItem> items = Arrays.asList(itemsElement);
-            InspectContentResponse response = dlp.inspectContent(inspectConfig, items);
 
-            List<InspectResult> resultsList = response.getResultsList();
+            String inspectResponse = callInspectContent(dlp, inspectConfig, items);
+            String maskValue = callDeidentify(dlp, inspectConfig, items);
 
-            Gson gson = new Gson();
-            String json = gson.toJson(resultsList);
             TableRow row = new TableRow();
-            row.put("DLPResponse", json);
-            logger.info("dlp-response=" + json);
+            row.put("Id", id);
+            row.put("Body", message);
+            row.put("MaskBody", maskValue);
+
             c.output(row);
         }  catch (Throwable e) {
             throw new RuntimeException(e);
@@ -58,33 +54,72 @@ public class DataLossPreventionFn extends DoFn<TableRow, TableRow> {
                 }
             }
         }
-//        try (DlpServiceClient dlp = DlpServiceClient.create()) {
-//            String name = "EMAIL_ADDRESS";
-//            InfoType infoTypesElement = InfoType.newBuilder()
-//                    .setName(name)
-//                    .build();
-//            List<InfoType> infoTypes = Arrays.asList(infoTypesElement);
-//            InspectConfig inspectConfig = InspectConfig.newBuilder()
-//                    .addAllInfoTypes(infoTypes)
-//                    .build();
-//            String type = "text/plain";
-//            //String value = "My email is example @example.com.";
-//            ContentItem itemsElement = ContentItem.newBuilder()
-//                    .setType(type)
-//                    .setValue(message)
-//                    .build();
-//            List<ContentItem> items = Arrays.asList(itemsElement);
-//            InspectContentResponse response = dlp.inspectContent(inspectConfig, items);
-//
-//            List<InspectResult> resultsList = response.getResultsList();
-//
-//            String json = m.writeValueAsString(resultsList);
-//            TableRow row = new TableRow();
-//            row.put("DLPResponse", json);
-//            logger.info("dlp-response=" + json);
-//            c.output(row);
-//        } catch (Throwable e) {
-//            throw new RuntimeException(e);
-//        }
+    }
+
+    private List<InfoType> getInfoTypeList() {
+        List<String> names = new ArrayList<>();
+        names.add("EMAIL_ADDRESS");
+        names.add("CREDIT_CARD_NUMBER");
+        names.add("FIRST_NAME");
+        names.add("LAST_NAME");
+        names.add("IBAN_CODE");
+        names.add("LOCATION");
+        names.add("PERSON_NAME");
+        names.add("PHONE_NUMBER");
+        names.add("SWIFT_CODE");
+        names.add("JAPAN_INDIVIDUAL_NUMBER");
+        names.add("JAPAN_PASSPORT");
+
+        List<InfoType> infoTypes = new ArrayList<>();
+        for (String name : names) {
+            InfoType infoTypesElement = InfoType.newBuilder()
+                    .setName(name)
+                    .build();
+            infoTypes.add(infoTypesElement);
+        }
+
+        return infoTypes;
+    }
+
+    private String callInspectContent(DlpServiceClient dlp, InspectConfig inspectConfig, List<ContentItem> items) {
+        InspectContentResponse response = dlp.inspectContent(inspectConfig, items);
+        List<InspectResult> resultsList = response.getResultsList();
+        List<Finding> findings = new ArrayList<>();
+        for (InspectResult result : resultsList) {
+            for (Finding finding : result.getFindingsList()) {
+                findings.add(finding);
+            }
+        }
+
+        Gson gson = new Gson();
+        return gson.toJson(findings);
+    }
+
+    private String callDeidentify(DlpServiceClient dlp, InspectConfig inspectConfig, List<ContentItem> items) {
+        CharacterMaskConfig maskConfig = CharacterMaskConfig.newBuilder().setMaskingCharacter("*").build();
+        PrimitiveTransformation primitiveTransformation = PrimitiveTransformation.newBuilder().setCharacterMaskConfig(maskConfig).build();
+        InfoTypeTransformations.InfoTypeTransformation infoTypeTransformation = InfoTypeTransformations.InfoTypeTransformation.newBuilder().setPrimitiveTransformation(primitiveTransformation).build();
+        InfoTypeTransformations infoTypeTransformations = InfoTypeTransformations.newBuilder().addTransformations(infoTypeTransformation).build();
+        DeidentifyConfig deidentifyConfig = DeidentifyConfig.newBuilder().setInfoTypeTransformations(infoTypeTransformations).build();
+
+        DeidentifyContentResponse deidentifyContentResponse = dlp.deidentifyContent(deidentifyConfig, inspectConfig, items);
+        List<ContentItem> itemsList = deidentifyContentResponse.getItemsList();
+        if (itemsList.size() > 0) {
+            return itemsList.get(0).getValue();
+        }
+        return "";
+    }
+
+    private Long parseLong(Object value) {
+        if (value == null) {
+            return null;
+        }
+
+        try {
+            return Long.parseLong(value.toString());
+        } catch (NumberFormatException e) {
+            logger.info(String.format("failed ParseLong; value = %s", value));
+            return null;
+        }
     }
 }
--- src/main/java/org/sinmetal/mlapi/MLApi.java
+++ src/main/java/org/sinmetal/mlapi/MLApi.java
@@ -15,6 +15,21 @@ import org.apache.beam.sdk.values.PCollection;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableReference;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MLApi {
 
     public interface CleansingOptions extends GcpOptions {
@@ -32,18 +47,20 @@ public class MLApi {
         Pipeline p = Pipeline.create(options);
 
         TableReference table = new TableReference();
-        table.setProjectId("sinmetal");
-        table.setDatasetId("hoge");
-        table.setTableId("MessageShort");
+        table.setProjectId("sinmetal-samples");
+        table.setDatasetId("dlp");
+        table.setTableId("Message");
 
         List<TableFieldSchema> fields = new ArrayList<>();
-        fields.add(new TableFieldSchema().setName("DLPResponse").setType("STRING"));
+        fields.add(new TableFieldSchema().setName("Id").setType("INTEGER"));
+        fields.add(new TableFieldSchema().setName("Body").setType("STRING"));
+        fields.add(new TableFieldSchema().setName("MaskBody").setType("STRING"));
         TableSchema schema = new TableSchema().setFields(fields);
 
         TableReference tableRef = new TableReference();
-        tableRef.setProjectId("sinmetal");
-        tableRef.setDatasetId("hoge");
-        tableRef.setTableId("DLPMessageShort");
+        tableRef.setProjectId("sinmetal-samples");
+        tableRef.setDatasetId("dlp");
+        tableRef.setTableId("DLPMessage");
 
         p.apply(BigQueryIO.readTableRows().from(table))
                 .apply("DLP API", new DataLossPrevention())
